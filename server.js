ce718597d3c164555d86cd8a06d8f40dd3e8af2d
nanisirangu <92352974+nanisirangu@users.noreply.github.com>
2026-01-11 03:21:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,26 +13,17 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/work", require("./routes/work"));
 app.use("/api/admin", require("./routes/admin"));
 
-app.get("/api/test-db", async (req, res) => {
-  try {
-    const r = await pool.query("SELECT NOW()");
-    res.json({ db_time: r.rows[0] });
-  } catch (err) {
-    console.error("DB ERROR:", err.message);
-    res.status(500).json({ error: "DB connection failed" });
-  }
-});
+const PORT = process.env.PORT || 3000;
 app.get("/api/test-db", async (req, res) => {
   try {
     const r = await pool.query("SELECT NOW()");
     res.json({ db_time: r.rows[0].now });
   } catch (err) {
-    console.error(err);
+    console.error("DB ERROR:", err.message);
     res.status(500).json({ error: "DB connection failed" });
   }
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Backend running on port", PORT);
 });
